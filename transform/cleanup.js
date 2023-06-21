03e7594007e8ece58ddd616b777f84f9b1e6bccc
Alexander Miller <alexanderm781@gmail.com>
2023-06-21 16:08:54 +1200

    Populate missing names

--- transform/cleanup.js
+++ transform/cleanup.js
@@ -9,8 +9,10 @@ const __dirname = path.dirname(__filename);
 export const cleanupInscriptionFiles = async () => {
   for(let collection of getDirectories(path.resolve(__dirname, '../collections/'))) {
     console.log(`🧹 Cleaning ${collection}`);
-    let filePath = `../collections/${collection}/inscriptions.json`;
-    let inscriptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, filePath)));
+    let filePathInscriptions = `../collections/${collection}/inscriptions.json`;
+    let inscriptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, filePathInscriptions)));
+    let filePathMeta = `../collections/${collection}/meta.json`;
+    let meta = JSON.parse(fs.readFileSync(path.resolve(__dirname, filePathMeta)));
 
     // Transform 'number' params into integers for consistency
     for (let i=0; i < inscriptions.length; i+=1) {
@@ -19,11 +21,12 @@ export const cleanupInscriptionFiles = async () => {
       }
 
       if(inscriptions[i].id) inscriptions[i].id = inscriptions[i].id.trim();
+      if(!inscriptions[i].meta) inscriptions[i].meta = { name: meta.name+" #"+i };
     }
 
     // Compress JSON to remove newlines
     const outputJson = JSON.stringify(inscriptions, null, null);
 
-    fs.writeFileSync(path.resolve(__dirname, filePath), outputJson);
+    fs.writeFileSync(path.resolve(__dirname, filePathInscriptions), outputJson);
   }
 };
