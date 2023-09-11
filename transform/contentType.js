dba3da742e78b28f513b0acba55f3d2d0092696b
anatolie <contact@anatolie.me>
2023-09-11 15:22:53 -0400

    Fix sub10k content_type fetching

--- transform/contentType.js
+++ transform/contentType.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const BATCH_SIZE = 60;
 const BATCHES_BEFORE_DELAY = 750;
 const BATCH_TIMEOUT_DELAY = 60000;
 
@@ -17,6 +18,12 @@ const COLLECTION_WIDE_OVERRIDES = {
 const ITEM_WIDE_OVERRIDES = {
   '50e74f16f8f4f18c29e572ed466dde40a0878f30db6745467841e73b2f96ab34i0': 'video/mp4', // Fixes only first item in twelvefold being video/mp4
 };
+
+// These collections have multiple content types, so we can't rely on the first inscription
+// to set content_type for the collection inscriptions
+const MULTI_TYPE_COLLECTIONS = [
+  'sub10k',
+];
 
 let currentBatchCount = 0;
 
@@ -46,26 +53,64 @@ export const addContentType = async () => {
     console.error('Error reading environment.json', e);
   }
 
-  for(let collection of getDirectories(path.resolve(__dirname, '../collections/'))) {
-    console.log(`🌅 Adding content_type to ${collection}`);
-    let filePath = `../collections/${collection}/inscriptions.json`;
+  const getInscriptionTypesPaginated = async (collection, inscriptions = [], page = 1) => {
+    for (let i = 0; i < inscriptions.length; i += BATCH_SIZE) {
+      const chunk = inscriptions.slice(i, i + BATCH_SIZE);
 
-    let inscriptions = [];
-    try {
-      inscriptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, filePath)));
-    } catch (e) {
-      console.error(e);
+      console.log(`🌠 Getting inscription type chunk ${Math.floor(i / 60) + 1}/${Math.ceil(inscriptions.length / BATCH_SIZE)} for ${collection}`)
+
+      const inscriptionIds = chunk.map((i) => i.id);
+      const inscriptionIdsString = inscriptionIds.map((id) => `&id=${id}`).join('');
+
+      let json;
+      let failed = false;
+      try {
+        json = await fetch(`https://api.hiro.so/ordinals/v1/inscriptions?limit=${BATCH_SIZE}${inscriptionIdsString}`, {
+          headers: {
+            'x-hiro-api-key': environment?.HIRO_API_KEY ?? null,
+          }
+        }).then(res => res.json());
+      } catch {
+        failed = true;
+        console.log(`!! Failed`);
+      }
+
+      if (!failed && json) {
+        for (let i=0; i < json.results?.length; i+=1) {
+          const result = json.results[i];
+          const inscriptionIndex = inscriptions.findIndex((i) => i.id === result?.id);
+          if (inscriptionIndex > -1 && result?.mime_type) {
+            inscriptions[inscriptionIndex].content_type = result.mime_type;
+          }
+        }
+      } else {
+        console.log(`Failed to get inscription types!`)
+      }
+
+      currentBatchCount += 1;
+
+      if ((currentBatchCount % BATCHES_BEFORE_DELAY) === 0) {
+        console.log(`==== Hit ${BATCHES_BEFORE_DELAY} batches, waiting ${BATCH_TIMEOUT_DELAY}ms before continuing...`)
+        await timeout(BATCH_TIMEOUT_DELAY);
+      }
     }
 
-    let task = async (inscriptions) => {
-      // Skip inscriptions that already contain a content_type
-      const inscriptionsToProcess = inscriptions.filter((i) => !i.content_type || i.content_type?.length === 0);
+    return inscriptions;
+  };
 
-      if (inscriptionsToProcess.length > 0) {
-        // IMPORTANT: for efficiency this script will take only the first inscription in a collection and assume all items
-        //            within that collection use that mime_type
-        let contentType = null;
+  const task = async (collection, inscriptions) => {
+    // Skip inscriptions that already contain a content_type
+    const inscriptionsToProcess = inscriptions.filter((i) => !i.content_type || i.content_type?.length === 0);
 
+    if (inscriptionsToProcess.length > 0) {
+      // IMPORTANT: for efficiency this script will take only the first inscription in a collection and assume all items
+      //            within that collection use that mime_type
+      let contentType = null;
+
+      // Multi content_type collection (gets content_type of each item individually)
+      if (MULTI_TYPE_COLLECTIONS.includes(collection)) {
+        return await getInscriptionTypesPaginated(collection, inscriptions);
+      } else { // Rely on first content_type of collection to set others (for efficiency)
         let json;
         let failed = false;
         try {
@@ -102,21 +147,33 @@ export const addContentType = async () => {
             };
           }
         }
-
-        currentBatchCount += 1;
-
-        if ((currentBatchCount % BATCHES_BEFORE_DELAY) === 0) {
-          console.log(`==== Hit ${BATCHES_BEFORE_DELAY} batches, waiting ${BATCH_TIMEOUT_DELAY}ms before continuing...`)
-          await timeout(BATCH_TIMEOUT_DELAY);
-        }
-
-        console.log(`🤖 Processing batch ${currentBatchCount} (content_type)`);
       }
 
-      return inscriptions;
-    };
+      currentBatchCount += 1;
 
-    let transformedInscriptions = await task(inscriptions);
+      if ((currentBatchCount % BATCHES_BEFORE_DELAY) === 0) {
+        console.log(`==== Hit ${BATCHES_BEFORE_DELAY} batches, waiting ${BATCH_TIMEOUT_DELAY}ms before continuing...`)
+        await timeout(BATCH_TIMEOUT_DELAY);
+      }
+
+      console.log(`🤖 Processing batch ${currentBatchCount} (content_type)`);
+    }
+
+    return inscriptions;
+  };
+
+  for(let collection of getDirectories(path.resolve(__dirname, '../collections/'))) {
+    console.log(`🌅 Adding content_type to ${collection}`);
+    let filePath = `../collections/${collection}/inscriptions.json`;
+
+    let inscriptions = [];
+    try {
+      inscriptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, filePath)));
+    } catch (e) {
+      console.error(e);
+    }
+
+    let transformedInscriptions = await task(collection, inscriptions);
     fs.writeFileSync(path.resolve(__dirname, filePath), JSON.stringify(transformedInscriptions, null, null));
   }
 };
